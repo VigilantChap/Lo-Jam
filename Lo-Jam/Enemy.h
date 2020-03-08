4f7fb2178615378c2cb281f7c25502f04e207e19
Richard Dela Merced <Delamercedrichard@gmail.com>
2020-03-07 21:01:20 -0500

    Enemy STUFF

--- Lo-Jam/Enemy.h
+++ Lo-Jam/Enemy.h
@@ -2,20 +2,36 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 #include "Entity.h"
+#include <random>
+
 class Enemy : public Entity
 {
+
+public:
+	static sf::Clock timelapse;
+	static std::default_random_engine pgenerator;
+private:
+	sf::IntRect sourceRectImage;
+	sf::Vector2<float> playerPosition;
+	bool isTriggered;
+	bool isVisible;
+	class Camera *camera;
 public:
 	Enemy(std::string ID);
 	~Enemy(); 
 
 	void Update();
 	
-	static sf::Clock timelapse;
+	
 	void SetPlayerPosition(sf::Vector2<float>);
+
+	void StartPatrolMovement();
+	void ToggleVisibility();
+
+
 private:
-	sf::IntRect sourceRectImage;
-	sf::Vector2<float> playerPosition;
-	bool isTriggered;
+	void Animate();
+
 };
 
 
